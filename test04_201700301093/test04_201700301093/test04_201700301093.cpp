faa422dc1181c108c40413995aa7c4865495c6c2
null1998 <1050510316@qq.com>
2018-11-19 21:46:32 +0800

    ok

--- test04_201700301093/test04_201700301093/test04_201700301093.cpp
+++ test04_201700301093/test04_201700301093/test04_201700301093.cpp
@@ -63,80 +63,81 @@ int comparePriority(char a,char b) {
 	}
 	return pri[i][j];
 }
-char* toRPN(char* a) {
-	LinkStack<char>* s=new LinkStack<char>();
-	int j=0;
-	char* b = new char[j];
+int cal(char op, char num1, char num2) {
+	int result = 0;
+	switch (op) {
+	case '+':
+		result = num1+num2;
+		break;
+	case '-':
+		result = num1-num2;
+		break;
+	case '*':
+		result = num1*num2;
+		break;
+	case '/':
+		result = num1/num2;
+		break;
+	}
+	return result;
+}
+int RPN(char* a) {
+	LinkStack<int>* nums=new LinkStack<int>();
+	LinkStack<char>* ops=new LinkStack<char>();
 	for (int i = 0; i < 100 && a[i] != NULL; i++) {
 		if (a[i] != '+'&&a[i] != '-'&&a[i] != '*'&&a[i] != '/'&&a[i] != '('&&a[i] != ')') {
-			b[j] = a[i];
-			j++;
+			nums->push(a[i]-48);
 		}
 		else {
-				if (s->isEmpty()|| a[i] == '(') {
-					s->push(a[i]);
+				if (ops->isEmpty()|| a[i] == '(') {
+					ops->push(a[i]);
 				}
 				else if (a[i] == ')') {
-					while (s->getTop() != '(') {
-						b[j] = s->pop();
-						j++;
+					while (ops->getTop() != '(') {
+						char op= ops->pop();
+						char num2 = nums->pop();
+						char num1 = nums->pop();
+						nums->push(cal(op, num1, num2));
 					}
-					s->pop();
+					ops->pop();
 				}
 				else {
-					int priority=comparePriority(s->getTop(),a[i]);
+					int priority=comparePriority(ops->getTop(),a[i]);
 					if (priority == 1) {
-						s->push(a[i]);
-
-
+						ops->push(a[i]);
 					}
 					if (priority == 2 || priority == 3) {
-						while (!s->isEmpty()&&comparePriority(s->getTop(), a[i]) != 1) {
-							b[j] = s->pop();
-							j++;
+						while (!ops->isEmpty()&&comparePriority(ops->getTop(), a[i]) != 1) {
+							char op=ops->pop();
+							char num2 = nums->pop();
+							char num1 = nums->pop();
+							nums->push(cal(op, num1, num2));
 						}
+						ops->push(a[i]);
 					}
 				}
 		}
 
 	}
-	while(!s->isEmpty()) {
-		b[j] = s->pop();
-		j++;
+	while(!ops->isEmpty()) {
+		char op = ops->pop();
+		char num2 = nums->pop();
+		char num1 = nums->pop();
+		nums->push(cal(op, num1, num2));
 	}
-	return b;
+	int result=nums->getTop();
+	return result;
 }
-int calculate(char* b) {
-	LinkStack<char>* num=new LinkStack<char>();
-	int tmp=0;
-	for (int i = 0; i < 100 && b[i] != NULL; i++) {
-		switch (b[i]) {
-		 case '+':
-			 tmp=(num->pop() + 0) + (num->pop() + 0);
-			 num->push(tmp);
-			 continue;
-		 case '-':
-			 tmp = (num->pop() + 0) - (num->pop() + 0);
-			 num->push(tmp);
-			 break;
-		 case '*':
-			 tmp = (num->pop() + 0) * (num->pop() + 0);
-			 num->push(tmp);
-			 break;
-		 case '/':
-			 tmp = (num->pop() + 0) / (num->pop() + 0);
-			 num->push(tmp);
-			 break;
-		 default:
-			 num->push(b[i]);
-			 break;
-		}
-	}
-	return tmp;
-}
+
 int main() {
-	char* a = "2+3*(4+5)-6/4";
-	char* b = toRPN(a);
-	cout << calculate(b);
+	//2+3*(4+5)-6/4
+	//9+(3+5)/2-2*3
+	//2+3*4*(3+3)/2-9+8
+	char a[100];
+	cout << "Input"<<endl;
+	cin >> a;
+	cout << "Output" << endl;
+	cout << RPN(a)+0<<endl<<"End"<<endl;
 	system("pause");
+	return 0;
 }
